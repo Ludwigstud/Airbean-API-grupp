8b0722360ae99dcad367e710928b5c85813c09e0
alexander-p1 <alexanderpettersson_@hotmail.com>
2025-04-14 14:59:27 +0200

    orderjs ändringar

--- models/Order.js
+++ models/Order.js
@@ -3,25 +3,25 @@ import path from 'path';
 
 const orderDB = new Datastore({ filename: path.join(__dirname, 'db', 'orders.db'), autoload: true });
 
-createOrder: (orderData, callback) => {
+const createOrder = (orderData, callback) => {
     orderDB.insert(orderData, (err, newDoc) => {
         if(err) return callback(err);
         callback(null, newDoc);
     });
 };
 
-getAllOrders: (callback) => {
+const getAllOrders = (callback) => {
     orderDB.find({}, (err, docs) => {
         if (err) return callback(err);
         callback(null, docs);
     });
 };
 
-deleteOrder: (id, callback) => {
+const deleteOrder = (id, callback) => {
     orderDB.remove({ _id: id }, {}, (err, numRemoved) => {
         if(err) return callback(err);
         callback(null, numRemoved);
     });
 };
 
-export default { createOrder, getAllOrders, updateOrder, deleteOrder }
+export default { createOrder, getAllOrders, deleteOrder }
